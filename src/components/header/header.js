3dfe8c7c3ada68f24a0c475c69e24157bf186d10
Maxim Talanov <mail@maxtalanov.ru>
2022-07-21 19:12:23 +0300

    refactoring:header добавить классы и исправить теги на более семантичные

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,12 @@
 import logo from '../../assets/logo.svg'
 import './header.scss';
+
 const Header = () => {
 
     return(
         <header className="header">
             <img className="header__logo" src={logo} alt="Логотип CIRCEYA"/>
-            <span >
-                <a href="tel:+74954954954">+7 (495) 495-49-54</a>
-            </span>
+            <a className="header__phone" href="tel:+74954954954">+7 (495) 495-49-54</a>
         </header>
     )
 }
