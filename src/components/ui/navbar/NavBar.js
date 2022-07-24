0c45802a61f7315bfb8c2422080c98c28b3e5166
Maxim Talanov <mail@maxtalanov.ru>
2022-07-24 06:19:57 +0300

    UI:feat - добавить компонент прототи навигиции

---
+++ src/components/ui/navbar/NavBar.js
@@ -0,0 +1,14 @@
+import "./Navbar.scss";
+
+const NavBar = () => {
+    return(
+        <nav className="navbar">
+            <ul className="navbar__list">
+                <li className="navbar__el navbar__el_active_border">Lorem</li>
+                <li className="navbar__el">Ipsum</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default NavBar;
